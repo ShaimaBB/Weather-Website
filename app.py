0d412363a87799b1b9b411d65aa4f33f8e481285
ShaimaBB <sbashar4488@gmail.com>
2025-01-26 21:51:15 -0330

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 import requests
 from flask_cors import CORS
+import os
+
 
 app = Flask(__name__)
 CORS(app)
@@ -36,4 +38,5 @@ def get_weather():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host='0.0.0.0', port=port, debug=True)
